0f6b2d37327908c529e25df8543a0981aa39f589
Prayas-35 <prayaspal04@gmail.com>
2025-10-26 04:48:57 +0530

    landing page

---
+++ client/app/_providers/wagmi.ts
@@ -0,0 +1,24 @@
+import { http, cookieStorage, createConfig, createStorage } from "wagmi";
+import { avalancheFuji, avalanche } from "wagmi/chains";
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+
+export function getConfig(connectors: ReturnType<typeof connectorsForWallets>) {
+    return createConfig({
+        chains: [avalancheFuji, avalanche],
+        connectors,
+        storage: createStorage({
+            storage: cookieStorage,
+        }),
+        ssr: true,
+        transports: {
+            [avalancheFuji.id]: http(),
+            [avalanche.id]: http(),
+        },
+    });
+}
+
+declare module "wagmi" {
+    interface Register {
+        config: ReturnType<typeof getConfig>;
+    }
+}
